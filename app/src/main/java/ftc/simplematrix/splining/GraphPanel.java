0895d55654375c81452c6d601ad92c7fd3e8d9d6
tarunja1ks <tarunjaiks@gmail.com>
2024-06-29 11:47:39 -0700

    thing

--- app/src/main/java/ftc/simplematrix/splining/GraphPanel.java
+++ app/src/main/java/ftc/simplematrix/splining/GraphPanel.java
@@ -1,5 +1,10 @@
 package ftc.simplematrix.splining;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.List;
 import javax.swing.JPanel;
 
 import org.ejml.equation.Symbol;
@@ -14,23 +19,16 @@ public class GraphPanel extends JPanel {
     private double c0, c1, c2, c3, c4, c5;
     private double x1,x2,y1,y2;
     private Image backgroundImage;
-    public GraphPanel(double c0, double c1, double c2, double c3, double c4, double c5, double x1, double x2, double y1, double y2) {
-        this.c0 = c0;
-        this.c1 = c1;
-        this.c2 = c2;
-        this.c3 = c3;
-        this.c4 = c4;
-        this.c5 = c5; 
-        this.x1=x1;
-        this.x2=x2;
-        this.y1=y1;
-        this.y2=y2;
+    public List<QSplinePath> spline_x;
+    public List<QSplinePath> spline_y;
+    public GraphPanel(List<QSplinePath> spline_x, List<QSplinePath> spline_y) {
+        this.spline_x=spline_x;
+        this.spline_y=spline_y;
         this.backgroundImage = new ImageIcon("src/main/java/ftc/simplematrix/splining/custom-centerstage-field-diagrams-works-with-meepmeep-v0-gytjsw5tfpob1.png").getImage();
-        
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.drawImage(this.backgroundImage, 0, 0, getWidth(), getHeight(), this);
@@ -69,32 +67,54 @@ public class GraphPanel extends JPanel {
         }
 
         // Draw the polynomial q(t) = c0 + c1*t + c2*t^2 + c3*t^3 + c4*t^4 + c5*t^5
-        g2d.setColor(Color.RED);
-        double prevX = 0, prevY = 0;
-        boolean firstPoint = true;
-
-        for (double x =x1; x <= x2; x += 0.1) { // Increase the step for smoothness
-            double t = x;
-            double y = c0 + c1 * t + c2 * Math.pow(t, 2) + c3 * Math.pow(t, 3) + c4 * Math.pow(t, 4) + c5 * Math.pow(t, 5);
-            double scaledX = yAxis + x * (width / 144.0);
-            double scaledY = xAxis - y * (height / 144.0); // Adjust scaling factor here
-            if(t==5){
-                System.out.println(y+" yayyyayayayyayya");
-            }
-            if (firstPoint) {
-                firstPoint = false;
-            } else {
-                g2d.drawLine((int) prevX, (int) prevY, (int) scaledX, (int) scaledY);
-            }
-
-            prevX = scaledX;
-            prevY = scaledY;
+        
+        for(int i=0;i<spline_y.size();i++){
+            graphSpline(g2d, spline_x.get(i), spline_y.get(i), xAxis, yAxis, width, height);
+            plotPoint(g2d, spline_x.get(i).q0, spline_y.get(i).q0);
+            plotPoint(g2d, spline_x.get(i).qf, spline_y.get(i).qf);
         }
 
 
+        // g2d.setColor(Color.BLACK);
+        // prevX = 0; prevY = 0;
+        // firstPoint = true;
+        // for (double t = 0; t <= 20000; t += 0.1) {
+        //     double xpos = t;
+        //     updatecoeffecientsplines(spline_y);
+        //     double ypos = c0 + c1 * t + c2 * Math.pow(t, 2) + c3 * Math.pow(t, 3) + c4 * Math.pow(t, 4) + c5 * Math.pow(t, 5);
+        //     double scaledX = yAxis + xpos * (width / 144.0);
+        //     double scaledY = xAxis - ypos * (height / 144.0);
 
-        plotPoint(g2d, x1, y1);
-        plotPoint(g2d, x2, y2);
+        //     if (firstPoint) {
+        //         firstPoint = false;
+        //     } else {
+        //         g2d.drawLine((int) prevX, (int) prevY, (int) scaledX, (int) scaledY);
+        //     }
+
+        //     prevX = scaledX;
+        //     prevY = scaledY;
+        // }
+        // g2d.setColor(Color.GREEN);
+        // prevX = 0; prevY = 0;
+        // firstPoint = true;
+        // for (double t = 0; t <= 20000; t += 0.1) {
+        //     double xpos = t;
+        //     updatecoeffecientsplines(spline_x);
+        //     double ypos = c0 + c1 * t + c2 * Math.pow(t, 2) + c3 * Math.pow(t, 3) + c4 * Math.pow(t, 4) + c5 * Math.pow(t, 5);
+        //     double scaledX = yAxis + xpos * (width / 144.0);
+        //     double scaledY = xAxis - ypos * (height / 144.0);
+
+        //     if (firstPoint) {
+        //         firstPoint = false;
+        //     } else {
+        //         g2d.drawLine((int) prevX, (int) prevY, (int) scaledX, (int) scaledY);
+        //     }
+
+        //     prevX = scaledX;
+        //     prevY = scaledY;
+        // }
+
+       
     }
 
 
@@ -107,5 +127,37 @@ public class GraphPanel extends JPanel {
 
         g2d.setColor(Color.BLUE);
         g2d.fillOval((int) scaledX - 5, (int) scaledY - 5, 10, 10); // Draw a small circle at the point
+    }
+
+    public void updatecoeffecientsplines(QSplinePath awesomepantssplines){ //updating c1 to 5 to the spline assigned so that way i dont call it each time and its not annoying and poopy
+        this.c0=awesomepantssplines.getcoeff().get(0);
+        this.c1=awesomepantssplines.getcoeff().get(1);
+        this.c2=awesomepantssplines.getcoeff().get(2);
+        this.c3=awesomepantssplines.getcoeff().get(3);
+        this.c4=awesomepantssplines.getcoeff().get(4);
+        this.c5=awesomepantssplines.getcoeff().get(5);
+    }
+
+    public void graphSpline(Graphics2D g2d, QSplinePath spline_x, QSplinePath spline_y, double xAxis, double yAxis, double width, double height){
+        g2d.setColor(Color.RED);
+        double prevX = 0, prevY = 0;
+        boolean firstPoint = true;
+        for (double t = spline_x.getTimes()[0]; t <= spline_x.getTimes()[1]; t += 0.1) {
+            updatecoeffecientsplines(spline_x);
+            double xpos = c0 + c1 * t + c2 * Math.pow(t, 2) + c3 * Math.pow(t, 3) + c4 * Math.pow(t, 4) + c5 * Math.pow(t, 5);
+            updatecoeffecientsplines(spline_y);
+            double ypos = c0 + c1 * t + c2 * Math.pow(t, 2) + c3 * Math.pow(t, 3) + c4 * Math.pow(t, 4) + c5 * Math.pow(t, 5);
+            double scaledX = yAxis + xpos * (width / 144.0);
+            double scaledY = xAxis - ypos * (height / 144.0);
+
+            if (firstPoint) {
+                firstPoint = false;
+            } else {
+                g2d.drawLine((int) prevX, (int) prevY, (int) scaledX, (int) scaledY);
+            }
+
+                prevX = scaledX;
+                prevY = scaledY;
+        }
     }
 }
